f228bad8e4beb5665dfb2c4b6dd1a3b62891642b
Thales <thalesondas0@gmail.com>
2024-09-25 10:11:50 -0300

    Melhorando tags globais

--- src/components/Objetivo.js
+++ src/components/Objetivo.js
@@ -2,18 +2,18 @@ import { Col, Container } from "react-bootstrap";
 
 const Objetivo = () => {
     return(
-        <>
+        <Container className="d-flex flex-column align-items-center">
             <h3>Objetivo</h3>
             <hr />
             <p>
-            Aplicar minhas habilidades em desenvolvimento de software em uma posição que permita contribuir para o sucesso da empresa. Busco oportunidades como:
+                Aplicar minhas habilidades em desenvolvimento de software em uma posição que permita contribuir para o sucesso da empresa. Busco oportunidades como:
             </p>
             <Container className="d-flex">
                 <Col><li>Desenvolvedor Full Stack</li></Col>
                 <Col><li>Desenvolvedor Back-end</li></Col>
             </Container>
             <li>Desenvolvedor Front-end</li>
-        </>
+        </Container>
     )
 }
 
