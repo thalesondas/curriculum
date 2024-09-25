94b932e42de76c239ad071a05527904c6276d210
Thales <thalesondas0@gmail.com>
2024-09-25 11:47:28 -0300

    Melhorando o Container de competências

--- src/components/Competencias.js
+++ src/components/Competencias.js
@@ -2,36 +2,38 @@ import { Col, Container } from "react-bootstrap";
 
 const Competencias = () => {
     return(
-        <>
-            <h3>Principais Competências / Idiomas</h3>
+        <Container className="mt-1">
+            <Container className="d-flex justify-content-start">
+                <h3>Principais Competências / Idiomas</h3>
+            </Container>
             <hr />
             <Container className="d-flex">
             <Col>
                 <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React.js</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React.js</li>
+                </ul>
+            </Col>
+            <Col style={{ transform: 'translateX(-22px)' }}>
+                <ul>
+                    <li>Node.js</li>
+                    <li>PostgreSQL</li>
+                    <li>MongoDB</li>
+                    <li style={{ whiteSpace: 'nowrap' }}>Versionamento Git/GitHub</li>
                 </ul>
             </Col>
             <Col>
                 <ul>
-                <li>Node.js</li>
-                <li>PostgreSQL</li>
-                <li>MongoDB</li>
-                <li>Versionamento Git/GitHub</li>
-                </ul>
-            </Col>
-            <Col>
-                <ul>
-                <li>React Bootstrap</li>
-                <li>Python</li>
-                <li>Java</li>
-                <li>Inglês Avançado</li>
+                    <li>React Bootstrap</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>Inglês Avançado</li>
                 </ul>
             </Col>
             </Container>
-        </>
+        </Container>
     )
 }
 
