f9a7f6fa0e6592c0747ea3111b3f0588cc2a3be9
Thales <thalesondas0@gmail.com>
2024-09-25 11:08:29 -0300

    Melhorando o Container do resumo

--- src/components/Resumo.js
+++ src/components/Resumo.js
@@ -1,15 +1,20 @@
+import { Container } from "react-bootstrap";
+
 const Resumo = () => {
     return(
-        <>
-            <h3>Resumo</h3>
+        <Container>
+            <Container className="mt-3 d-flex flex-column align-items-start">
+                <h3>Resumo</h3>
+            </Container>
             <hr />
-            <p>Estou me formando em Análise e Desenvolvimento de Sistemas pela UTFPR de
-            Cornélio Procópio, onde adquiri conhecimento em várias linguagens de
-            programação, bancos de dados, bibliotecas e frameworks, etc. Além de aprimorar
-            alguns desses conhecimentos e aprender novas tecnologias por meio de cursos
+            <p style={{ whiteSpace: 'pre-wrap', lineHeight: '1.4rem' }}>
+            Estou me formando em Análise e Desenvolvimento de Sistemas pela UTFPR de<br/>
+            Cornélio Procópio, onde adquiri conhecimento em várias linguagens de<br/>
+            programação, bancos de dados, bibliotecas e frameworks, etc. Além de aprimorar<br/>
+            alguns desses conhecimentos e aprender novas tecnologias por meio de cursos<br/>
             online gratuitos e da plataforma Udemy.
             </p>
-        </>
+        </Container>
     )
 }
 
