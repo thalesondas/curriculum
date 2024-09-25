f3c44c2b04f89bccc4d49ff8a5fe9f1c97484760
Thales <thalesondas0@gmail.com>
2024-09-25 10:57:39 -0300

    Melhorando o Container do objetivo

--- src/components/Objetivo.js
+++ src/components/Objetivo.js
@@ -2,17 +2,19 @@ import { Col, Container } from "react-bootstrap";
 
 const Objetivo = () => {
     return(
-        <Container className="d-flex flex-column align-items-center">
-            <h3>Objetivo</h3>
+        <Container className="mt-3 d-flex flex-column align-items-center">
+            <Container className="d-flex justify-content-start align-items-start">
+                <h3>Objetivo</h3>
+            </Container>
             <hr />
-            <p>
+            <p style={{ lineHeight: '1.3rem'}}>
                 Aplicar minhas habilidades em desenvolvimento de software em uma posição que permita contribuir para o sucesso da empresa. Busco oportunidades como:
             </p>
             <Container className="d-flex">
-                <Col><li>Desenvolvedor Full Stack</li></Col>
-                <Col><li>Desenvolvedor Back-end</li></Col>
+                <Col className="ms-5 d-flex justify-content-start"><li>Desenvolvedor Full Stack</li></Col>
+                <Col className="me-5"><li>Desenvolvedor Back-end</li></Col>
             </Container>
-            <li>Desenvolvedor Front-end</li>
+            <li className="mt-1 me-5">Desenvolvedor Front-end</li>
         </Container>
     )
 }
