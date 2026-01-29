9d9bed2ec67b69b850cb74f286a3e8302093fa20
Thales <thalesondas0@gmail.com>
2026-01-29 02:48:45 -0300

    update site CV header

--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -1,52 +1,23 @@
 import { Col, Container, Row, Image } from "react-bootstrap";
-import Linkedin from '../images/linkedin.svg'
-import Email from '../images/email.svg'
-import Git from '../images/git.svg'
-import Telefone from '../images/telefone.svg'
-import Site from '../images/site.svg'
 
 const Cabecalho = () => {
     return(
         <Container className="d-flex flex-column justify-content-center align-items-center">
-            <h1 className="mt-4">Thales Ondas</h1>
-            <h2 className="mb-4 text-uppercase">Desenvolvedor Full Stack</h2>
-            <Container className="mt-2 d-flex">
-            <Col className="d-flex justify-content-center">
-                <ul>
-                    <li className="mb-3 sem-bolinha">
-                        <Image className="img-svg" src={Telefone} alt="Telefone" />
-                        (19) 97125-0294
-                    </li>
-                    <li className="sem-bolinha">
-                        <Image className="img-svg" src={Email} alt="Email" />
-                        thalesondas0@gmail.com
-                    </li>
-                </ul>
-            </Col>
-            <Col className="d-flex justify-content-start">
-                <ul>
-                    <li className="mb-3 sem-bolinha">
-                        <Image className="img-svg" src={Linkedin} alt="LinkedIn" />
-                        <a className="custom-link" href="https://linkedin.com/in/thales-ondas" target="_blank" rel="noopener noreferrer">
-                            linkedin.com/in/thales-ondas
-                        </a>
-                    </li>
-                    <li className="sem-bolinha me-5">
-                        <Image className="img-svg" src={Git} alt="Git" />
-                        <a className="custom-link" href="https://github.com/thalesondas" target="_blank" rel="noopener noreferrer">
-                            github.com/thalesondas
-                        </a>
-                    </li>
-                </ul>
-            </Col>
-            </Container>
+            <h1 className="mt-5">Thales Ondas</h1>
+            <h2 className="mb-5">Desenvolvedor Full Stack</h2>
+            <Row className="mb-1">
+                <p className="ms-4 mb-0">
+                    <span className="ms-3">Americana - SP</span> |
+                    <span className="ms-4 me-4">(19) 97125-0294</span>
+                    <span className="ms-2">| thalesondas0@gmail.com</span>
+                </p>
+            </Row>
             <Row>
-                <li className="sem-bolinha">
-                    <Image className="img-svg" src={Site} alt="Site" />
-                    <a className="custom-link" href="https://portfolio-thalesondas.netlify.app" target="_blank" rel="noopener noreferrer">
-                        portfolio-thalesondas.netlify.app
-                    </a>
-                </li>
+                <p className="mb-0">
+                    <a href="https://linkedin.com/in/thales-ondas" target="_blank" rel="noreferrer" className="custom-link">linkedin.com/in/thales-ondas</a> | 
+                    <a href="https://github.com/thalesondas" target="_blank" rel="noreferrer" className="custom-link mx-1">github.com/thalesondas</a> | 
+                    <a href="https://portfolio-thalesondas.netlify.app/" target="_blank" rel="noreferrer" className="custom-link mx-1">portfolio-thalesondas.netlify.app/</a>
+                </p>
             </Row>
         </Container>
     )
